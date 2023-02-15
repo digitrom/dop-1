4b7519d9bedca777966f164087b0e56bca984059
digitrom <roma.bobrovski@gmail.com>
2023-02-15 18:38:47 +0300

    App - setting Filters

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React, {useState} from 'react';
 import './App.css';
 import {Country} from "./Country";
 
-export type BanknotsType = '' // создадим типы для banknotes -он может быть 'Dollars', 'RUBLS' или 'All'
+export type BanknotsType = 'Dollars' | 'RUBLS' | 'All' // создадим типы для banknotes -он может быть 'Dollars', 'RUBLS' или 'All'
 export type MoneyType = {
     banknotes: BanknotsType
-    value: any// не ленимся, убираем заглушку, и пишем правильный тип)
-    number: any// ложку за Димыча, за...
+    value: number// не ленимся, убираем заглушку, и пишем правильный тип)
+    number: string// ложку за Димыча, за...
 }
 
-let defaultMoney: any = [  // типизируем
+let defaultMoney: MoneyType[] = [  // типизируем
     {banknotes: 'Dollars', value: 100, number: ' a1234567890'},
     {banknotes: 'Dollars', value: 50, number: ' z1234567890'},
     {banknotes: 'RUBLS', value: 100, number: ' w1234567890'},
@@ -21,31 +21,49 @@ let defaultMoney: any = [  // типизируем
 ]
 
 // типизируем на входе и выходе
-export const moneyFilter = (money: any, filter: any): any => {
+export const moneyFilter = (money: MoneyType[], filterValue: BanknotsType): MoneyType[] => {
     //если пришел filter со значением 'All', то возвращаем все банкноты
     //return money.filter... ну да, придется фильтровать
+    // if (filter === 'Dollars') {
+    //     return money.filter(m => m.banknotes === 'Dollars')
+    // }
+    // if (filter === 'RUBLS') {
+    //     return money.filter(m => m.banknotes === 'RUBLS')
+    // } else {
+    //     return defaultMoney
+    // }
+    switch (filterValue) {
+        case 'Dollars':
+            return money.filter(m => m.banknotes === filterValue)
+        case 'RUBLS':
+            return money.filter(m => m.banknotes === filterValue)
+        default:
+            return money
+    }
 }
 
-function App() {
-    // убираем заглушки в типизации и вставляем в качестве инициализационного значения defaultMoney
-    const [money, setMoney] = useState<any>([])
-    const [filterValue, setFilterValue] = useState<any>('')   // по умолчанию указываем все банкноты
 
-    // а вот сейчас притормаживаем. И вдумчиво: константа filteredMoney получает результат функции moneyFilter
-    // в функцию передаем деньги и фильтр, по которому ихбудем выдавать(ретёрнуть)
-    const filteredMoney = moneyFilter(грошы, фильтръ)
-    return (
-        <div className="App">
-            <Country
-                data={filteredMoney}   //отрисовать будем деньги после фильтрации
-                setFilterValue={setFilterValue}  //useState передаем? Так можно было?!
 
-            />
-        </div>
-    );
-}
+    function App() {
+        // убираем заглушки в типизации и вставляем в качестве инициализационного значения defaultMoney
+        const [money, setMoney] = useState<MoneyType[]>(defaultMoney)
+        const [filterValue, setFilterValue] = useState<BanknotsType>('All')   // по умолчанию указываем все банкноты
+
+        // а вот сейчас притормаживаем. И вдумчиво: константа filteredMoney получает результат функции moneyFilter
+        // в функцию передаем деньги и фильтр, по которому ихбудем выдавать(ретёрнуть)
+        const filteredMoney = moneyFilter(money, filterValue)
+        return (
+            <div className="App">
+                <Country
+                    data={filteredMoney}   //отрисовать будем деньги после фильтрации
+                    setFilterValue={setFilterValue}  //useState передаем? Так можно было?!
+
+                />
+            </div>
+        );
+    }
 
 // Итого: в этой компоненте у нас мозги. А вот отрисовка где-то глубже. Погружаемся в Country
 
 
-export default App;
+    export default App;
